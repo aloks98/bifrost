90ffbc4c38dafc772d60a4b6ded8947860e20532
aloks98 <aloksahoo9819@gmail.com>
2022-06-27 12:57:33 +0530

    chore: Update response model

--- bifrost-server/models/response.go
+++ bifrost-server/models/response.go
@@ -1,7 +1,7 @@
 package models
 
 type Response struct {
-	Status int8        `json:"status"`
-	Error  string      `json:"error"`
+	Status int         `json:"status"`
+	Error  interface{} `json:"error"`
 	Data   interface{} `json:"data"`
 }
